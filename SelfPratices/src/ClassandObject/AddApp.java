96aa05f2ff915b66d9cc243bad122a07243eca84
holes <holes@Sanket>
2025-04-07 00:21:55 +0530

    My Revision

---
+++ SelfPratices/src/ClassandObject/AddApp.java
@@ -0,0 +1,25 @@
+package ClassandObject;
+import java.util.*;
+class Add
+{
+	Scanner sc=new Scanner(System.in);
+	int a,b;
+	void setValues(int x , int z)
+	{
+		a=x;
+		b=z;
+	}
+	void showAdd()
+	{
+		System.out.printf("Addition is : %d\n  ",a+b);
+	}
+}
+public class AddApp {
+
+	public static void main(String x[])
+	{
+		Add ad=new Add();
+		ad.setValues(100,200);
+		ad.showAdd();
+	}
+}
